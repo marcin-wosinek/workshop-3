dcbbf87d384c572590a61665c6d25e2237ba5921
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-05-15 17:43:05 +0200

    Read contact id from routeParams

--- app/scripts/controllers/contactView.js
+++ app/scripts/controllers/contactView.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('workshop2App')
-  .controller('ContactViewCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ContactViewCtrl', function ($scope, $routeParams) {
+    $scope.contact = $routeParams.id;
   });
