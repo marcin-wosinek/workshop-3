2f52ef1c69523ea79f54a365ef41dbaae9e44cb0
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-05-22 17:24:36 +0200

    Implement filter

--- app/scripts/filters/between.js
+++ app/scripts/filters/between.js
@@ -3,7 +3,8 @@
 angular.module('workshop2App')
   .filter('between', function () {
     /*
-     * TODO: implement filter which take 4 parameters
+     * filter which take 4 parameters
+     *
      * input - array of elements
      * key - property of each element, that is expected to fit between:
      * min - value
@@ -11,7 +12,20 @@ angular.module('workshop2App')
      *
      * example of usage app/views/showContacts.html
      */
-    return function (input) {
-      return 'between filter: ' + input;
+    return function (input, key, min, max) {
+      if (angular.isArray(input)) {
+        var toReturn = [];
+
+        angular.forEach(input, function (element) {
+          if (min <= element[key] && element[key] <= max) {
+            toReturn.push(element);
+          }
+        });
+
+        return toReturn;
+      }
+
+      // is not array - just return unmodified and forget
+      return input;
     };
   });
