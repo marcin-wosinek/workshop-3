fed5359d8f3903ccabfeed820ec2bd7239f32a94
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-05-29 20:09:55 +0200

    Hack: Block setting max below min, and min abox max

--- app/scripts/directives/ws-interval.js
+++ app/scripts/directives/ws-interval.js
@@ -6,8 +6,8 @@ angular.module('workshop2App')
      * controll for selecting min and max values for interval
      */
     return {
-      template: 'Min: <input type="range" max="{{max}}" min="{{min}}" ng-model="model.min"> {{model.min}}' +
-          ' <br> Max: <input type="range" max="{{max}}" min="{{min}}" ng-model="model.max"> {{model.max}}',
+      template: 'Min: <input type="range" max="{{model.max}}" min="{{min}}" ng-model="model.min"> {{model.min}}' +
+          ' <br> Max: <input type="range" max="{{max}}" min="{{model.min}}" ng-model="model.max"> {{model.max}}',
         scope: {
         min: '@',
         max: '@',
