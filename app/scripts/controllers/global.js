e8ab5a214d8e424a0403843e32241a447460450c
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-05-30 15:10:47 +0200

    Set tracking ids in cookies

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -1,12 +1,10 @@
 'use strict';
 
 angular.module('workshop2App')
-  // TODO inject cookies serivece & UUID generator
-  .controller('GlobalCtrl', function ($scope) {
-    // TODO: if it's empty set trackingId on cookie to new created UUID
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  // inject cookies serivece & UUID generator
+  .controller('GlobalCtrl', function ($scope, $cookies, wsUuidGenerator) {
+    // if it's empty set trackingId on cookie to new created UUID
+    if (!angular.isString($cookies.trackingId)) {
+      $cookies.trackingId = wsUuidGenerator.createUuid();
+    }
   });
