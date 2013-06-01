5d2582b05e212688bd7849cb1c1ba545c41b56ee
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-06-01 13:11:29 +0200

    Add ws-accept-cookies directive

---
+++ test/spec/directives/ws-accept-cookies.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Directive: wsAcceptCookies', function () {
+  beforeEach(module('workshop2App'));
+
+  var element;
+
+  it('should make hidden element visible', inject(function ($rootScope, $compile) {
+    element = angular.element('<ws-accept-cookies></ws-accept-cookies>');
+    element = $compile(element)($rootScope);
+    expect(element.text()).toBe('this is the wsAcceptCookies directive');
+  }));
+});
