6e4bd09f61e0ee77b5f11057e84459a896a8dafb
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-06-01 13:23:57 +0200

    Wrap ws-accept-cookies content

--- app/scripts/directives/ws-accept-cookies.js
+++ app/scripts/directives/ws-accept-cookies.js
@@ -3,13 +3,14 @@
 angular.module('workshop2App')
   .directive('wsAcceptCookies', function () {
     /*
-     * TODO: transclude original content in that directive, and add 'accept' button
+     * Transclude original content in that directive, and add 'accept' button
      */
     return {
-      template: '<div></div>',
-      restrict: 'E',
+      template: '<div ng-transclude></div>' +
+                '<button>Akceptuje</button>',
+      restrict: 'A',
+      transclude: true,
       link: function postLink(scope, element, attrs) {
-        element.text('this is the wsAcceptCookies directive');
       }
     };
   });
