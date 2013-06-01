217191686e21255d409153f7b2fb8758224851a7
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-06-01 16:53:45 +0200

    Add ws-shortcut-input

---
+++ test/spec/directives/ws-shortcut-input.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Directive: wsShortcutInput', function () {
+  beforeEach(module('workshop2App'));
+
+  var element;
+
+  it('should make hidden element visible', inject(function ($rootScope, $compile) {
+    element = angular.element('<ws-shortcut-input></ws-shortcut-input>');
+    element = $compile(element)($rootScope);
+    expect(element.text()).toBe('this is the wsShortcutInput directive');
+  }));
+});
