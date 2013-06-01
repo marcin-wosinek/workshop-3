22f86c1756af493798937305666e7d0f10033723
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-06-01 15:45:49 +0200

    Implement wsAcceptCookies

--- app/scripts/directives/ws-accept-cookies.js
+++ app/scripts/directives/ws-accept-cookies.js
@@ -1,19 +1,26 @@
 'use strict';
 
 angular.module('workshop2App')
-  .directive('wsAcceptCookies', function () {
+  .directive('wsAcceptCookies', function ($cookies) {
     /*
      * Transclude original content in that directive, and add 'accept' button
      */
     return {
       template: '<div ng-transclude></div>' +
-                '<button>Akceptuje</button>',
+                '<button ng-click="accept()">Akceptuje</button>',
       restrict: 'A',
       transclude: true,
-      link: function postLink(scope, element, attrs) {
-        // TODO: accept button logic
+      link: function postLink(scope, element, attrs)  {
+        // accept button logic
+        scope.accept = function () {
+          $cookies.accepted = 'true';
+          element.css('display', 'none');
+        };
 
-        // TODO: hide element if cookies are already accepted
+        // hide element if cookies are already accepted
+        if ($cookies.accepted) {
+          element.css('display', 'none');
+        }
       }
     };
   });
