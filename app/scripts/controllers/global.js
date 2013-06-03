8b18075eaa7752dc3eaccaf60a2786593675436d
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-06-03 13:13:59 +0200

    Add todo for tracking cookies

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('workshop2App')
+  // TODO inject cookies serivece & UUID generator
   .controller('GlobalCtrl', function ($scope) {
+    // TODO: if it's empty set trackingId on cookie to new created UUID
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
