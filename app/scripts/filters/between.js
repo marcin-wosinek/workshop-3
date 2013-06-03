8f51095052be8c8b7cceb8f5c541e8e2b173aa9d
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-06-03 09:53:02 +0200

    Usage of between filter

--- app/scripts/filters/between.js
+++ app/scripts/filters/between.js
@@ -2,6 +2,15 @@
 
 angular.module('workshop2App')
   .filter('between', function () {
+    /*
+     * TODO: implement filter which take 4 parameters
+     * input - array of elements
+     * key - property of each element, that is expected to fit between:
+     * min - value
+     * max - value
+     *
+     * example of usage app/views/showContacts.html
+     */
     return function (input) {
       return 'between filter: ' + input;
     };
