33d4b796d50b4e94f6ce42e852f7b861910a33f6
Marcin Wosinek <marcin.wosinek@gmail.com>
2013-08-04 18:13:53 +0200

    Failing test

--- test/spec/controllers/global.js
+++ test/spec/controllers/global.js
@@ -5,18 +5,28 @@ describe('Controller: GlobalCtrl', function () {
   // load the controller's module
   beforeEach(module('workshop2App'));
 
-  // TODO: add mocks for $cookies & wsUuidGenerator
   var GlobalCtrl,
-    scope;
+    scope,
+    cookies,
+    wsUuidGenerator;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    // TODO: set up mocks
+    // set up mocks
+    cookies = {};
+    wsUuidGenerator = {
+      createUuid: jasmine.createSpy('createUuid').andReturn('341')
+    };
+
     GlobalCtrl = $controller('GlobalCtrl', {
-      $scope: scope
+      $scope: scope,
+      $cookies: cookies,
+      wsUuidGenerator: wsUuidGenerator
     });
   }));
 
-  // TODO: create test with 'it'
+  it('should setup cookies', function () {
+    expect(cookies.trackingId).toEqual('341');
+  });
 });
